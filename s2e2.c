e436eb34cfc05e026c3664cfb8275467d5ee7812
jeffrocha <jeff.r.silva@hotmail.com>
2022-10-04 22:03:43 -0300

    adicionado comentario

--- s2e2.c
+++ s2e2.c
@@ -8,9 +8,11 @@ Faça um algoritmo para somar dois números e multiplicar o resultado pelo primeir
 #include <locale.h>
 // std = standard (padrão)
 // io = input/output (Entrada/Saída)
+//locale.h = biblioteca de região
 
 int main()// int é o valor que ira retornar da função main no caso irá retornar um inteiro
 {
+//Configuração de acentuação padrão brazil  
  setlocale(LC_ALL, "Portuguese_Brazil");
 //Variaveis tipo inteiro
 int num1, num2, soma, multiplicacao;
