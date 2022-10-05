28d096b00bec03bb5486ed7d8a0c7b11a345ea21
jeffrocha <jeff.r.silva@hotmail.com>
2022-10-04 22:02:06 -0300

    Adicionado resolução seção 2 exe02

---
+++ s2e2.c
@@ -0,0 +1,31 @@
+/*
+Seção 2 - ex 2
+
+Faça um algoritmo para somar dois números e multiplicar o resultado pelo primeiro número.
+*/
+
+#include<stdio.h>
+#include <locale.h>
+// std = standard (padrão)
+// io = input/output (Entrada/Saída)
+
+int main()// int é o valor que ira retornar da função main no caso irá retornar um inteiro
+{
+ setlocale(LC_ALL, "Portuguese_Brazil");
+//Variaveis tipo inteiro
+int num1, num2, soma, multiplicacao;
+
+//entradas
+printf("Informe o primeiro número: ");
+scanf("%d", &num1);
+
+printf("Informe o segundo número: ");
+scanf("%d", &num2);
+
+//processamento 
+soma= num1 + num2;
+multiplicacao= soma * num1;
+
+//saida
+printf("o resultado é %d", multiplicacao);
+}
