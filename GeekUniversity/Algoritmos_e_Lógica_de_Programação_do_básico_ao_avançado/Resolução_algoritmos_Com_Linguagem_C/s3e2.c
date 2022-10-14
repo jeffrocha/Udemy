e68a979967bb7df34a54f2ae55432d5ece5600af
jeffrocha <jeff.r.silva@hotmail.com>
2022-10-14 18:31:01 -0300

    organização e criação s3e2

---
+++ GeekUniversity/Algoritmos_e_Lógica_de_Programação_do_básico_ao_avançado/Resolução_algoritmos_Com_Linguagem_C/s3e2.c
@@ -0,0 +1,34 @@
+/*
+Faça um algoritmo para 'calcular o estoque médio de uma peça sendo que:
+
+estoque_medio = (quantidade_minima + quantidade_maxima) /1
+
+"Calcular o estoque médio"
+entrada
+    receber quantidade_minima
+    receber quantidade_maxima
+processar
+    somar quantidade_minima e quantidade_maxima
+    dividir o resultado da soma por 2
+saÃ­da
+    Mostrar o resultado da divisão
+*/
+#include <stdio.h>
+#include <locale.h>
+
+int main(void)
+{
+    // Entrada
+    setlocale(LC_ALL, "Portuguese_Brazil");
+    int Qtd_min, Qtd_max;
+    float Vardiv;
+
+    printf("Informe a quantidade Mínima: ");
+    scanf("%d", &Qtd_min);
+    printf("Informe a quantidade Máxima: ");
+    scanf("%d", &Qtd_max);
+    // processamento
+    Vardiv = (Qtd_min + Qtd_max) / 2;
+    // saí­da
+    printf("O estoque médio é: %.2f", Vardiv); // %.2F o númerador é quantidade de casa após a virgula
+}
