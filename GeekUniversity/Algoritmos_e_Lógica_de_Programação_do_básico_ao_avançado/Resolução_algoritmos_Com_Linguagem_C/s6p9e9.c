c098336e8e2c40b4669e88cbb52210b404199f8a
jeffrocha <jeff.r.silva@hotmail.com>
2022-10-17 20:08:05 -0300

    criação do arquivo / pergunta s6p9e9

---
+++ GeekUniversity/Algoritmos_e_Lógica_de_Programação_do_básico_ao_avançado/Resolução_algoritmos_Com_Linguagem_C/s6p9e9.c
@@ -0,0 +1,12 @@
+/*
+seção 6- exercício 9
+
+A secretaria do meio ambiente que controla o indice de poluição mantém 3 grupos de industrias que são altamente poluentes do meio ambiente.
+O indice de poluição aceitavel varia de 0,05 até 0,025.
+- se o indice sobe para 0,3 as indústrias do 1º grupo são intimadas a suspenderer suas atividades;
+- se o indice crescer para 0,4 as industrias do 1º e 2º grupos são indimadas a suspenderem suas atividades;
+- se o indice atingir 0,5 todos os grupos devem ser notificados a paralisarem suas atividades.
+faça um algoritmo que leia o indice de poluição medido e emita a notificação adequada aos diferentes grupos de empresas.
+
+
+*/
