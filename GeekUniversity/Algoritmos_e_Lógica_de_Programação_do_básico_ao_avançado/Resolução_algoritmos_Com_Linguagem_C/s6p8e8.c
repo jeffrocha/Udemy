55cdc41e1ccee3f061193dbad991979250aaa243
jeffrocha <jeff.r.silva@hotmail.com>
2022-10-17 19:59:15 -0300

    Seção 6 - Exercício 8

--- GeekUniversity/Algoritmos_e_Lógica_de_Programação_do_básico_ao_avançado/Resolução_algoritmos_Com_Linguagem_C/s6p8e8.c
+++ GeekUniversity/Algoritmos_e_Lógica_de_Programação_do_básico_ao_avançado/Resolução_algoritmos_Com_Linguagem_C/s6p8e8.c
@@ -2,5 +2,39 @@
 Seção 6 - Exercício 8
 Faça um algoritmo que leia um número inteiro e mostre uma mensagem
 indicando se este número é par ou impar, se é positivo ou negativo.
-n1 % 2 == 0
 */
+
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese,Brazil");
+    int Num1;
+    char NumIP[6];
+    char NumPN[9];
+
+    printf("Digite um valor: ");
+    scanf("%d", &Num1);
+
+    if (Num1 > 0)
+    {
+        strcpy(NumPN, "Positivo");
+    }
+    else
+    {
+        strcpy(NumPN, "Negativo");
+    }
+    if (Num1 % 2 == 0)
+    {
+        strcpy(NumIP, "Par");
+    }
+    else
+    {
+        strcpy(NumIP, "Impar");
+    }
+    printf("O número é %s %s", NumIP, NumPN);
+
+    return (0);
+}
