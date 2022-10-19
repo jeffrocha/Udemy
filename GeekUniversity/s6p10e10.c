1b0aaf2874f4ab94cebd6e09f078e2da148b0be3
jeffrocha <jeff.r.silva@hotmail.com>
2022-10-18 22:00:01 -0300

    notação do exercicio s6p10e10

---
+++ GeekUniversity/s6p10e10.c
@@ -0,0 +1,12 @@
+/*
+seção 6 - Exercicio 10
+
+Elabore um algoritmo que dada a dade de um nadador classigique-o em uma das seguitnes categorias;
+infantil -a = 5 a 7 anos
+infantil -b = 8 a 11 anos
+juvenil -a = 12 a 13 anos
+juvenil -b = 14 a 17 anos
+adulto = Igual ou maior que 18 anos
+
+
+*/
