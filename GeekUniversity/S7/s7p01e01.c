32e6efed474b0f498f1565df02202da1c1abf8e3
jeffrocha <jeff.r.silva@hotmail.com>
2022-10-21 16:09:30 -0300

    s7p01e01

---
+++ GeekUniversity/S7/s7p01e01.c
@@ -0,0 +1,29 @@
+/*
+Seção 07 - exercício 01
+
+Faça um algoritmo que determine o maior entre N números.
+A condição de para é a entrada de um valor 0(zero), ou seja, o algoritmo deve ficar calculando o maior até que a entrada seja igual a
+0 zero.
+
+*/
+
+#include <stdio.h>
+#include <locale.h>
+
+int main(void)
+{
+    setlocale(LC_ALL, "Portuguese_Brazil");
+    int ValMaior, Index;
+    ValMaior = 0;
+    Index = 0;
+    do
+    {
+        printf("Digite um número: ");
+        scanf("%d", &Index);
+        if (Index >= ValMaior)
+        {
+            ValMaior = Index;
+        }
+    } while (Index != 0);
+    printf("O maior número é %d", ValMaior);
+}
