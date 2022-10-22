9416d238c41be03960bc1966ddec854ac0585e36
jeffrocha <jeff.r.silva@hotmail.com>
2022-10-22 01:39:16 -0300

    s7p06e06

---
+++ GeekUniversity/Algoritmos_e_Lógica_de_Programação_do_básico_ao_avançado/Resolução_algoritmos_Com_Linguagem_C/S7/s7p06e06.c
@@ -0,0 +1,34 @@
+/*
+Seção 07 - Exercício 06
+
+Desenvolva um gerador de tabuada, capaz de gerar a tabuada de qualquer número inteiro entre 1 a 10.
+O usuário deve informar de qual número ele deseja ver a tabuada.
+A saída deve ser conforme o exemplo abaixo:
+
+Tabuada de 5:
+5 x 1  = 5
+5 x 2  = 10
+...
+5 x 10 = 50
+
+*/
+
+#include <stdio.h>
+#include <locale.h>
+
+int main(void)
+{
+    setlocale(LC_ALL, "Portuguese_Brazil");
+    int ValNum, ValMult, ValRep, ValCalc;
+    ValNum = ValMult = ValRep = ValCalc = 0;
+    printf("\nDigite o número da tabuada deseja exibir: ");
+    scanf("%d", &ValNum);
+    printf("\nDigite o número que deseja que a tabuada vá: ");
+    scanf("%d", &ValMult);
+    do
+    {
+        ValCalc = ValNum * ValRep;
+        printf("%d x %d = %d\n", ValNum, ValRep, ValCalc);
+        ValRep = ValRep + 1;
+    } while (ValRep < (ValMult + 1));
+}
