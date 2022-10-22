67bc5e820711357137545dd52fcdcfe9dbd0495a
jeffrocha <jeff.r.silva@hotmail.com>
2022-10-22 17:45:42 -0300

    s7p07e07

--- GeekUniversity/Algoritmos_e_Lógica_de_Programação_do_básico_ao_avançado/Resolução_algoritmos_Com_Linguagem_C/S7/s7p07e07.c
+++ GeekUniversity/Algoritmos_e_Lógica_de_Programação_do_básico_ao_avançado/Resolução_algoritmos_Com_Linguagem_C/S7/s7p07e07.c
@@ -25,3 +25,72 @@ Situação                                   Qtd      Porcentual
 4-Quebrado ou inutilizado                   15          15%
 
 */
+#include <stdio.h>
+#include <locale.h>
+
+int main(void)
+{
+    setlocale(LC_ALL, "Portuguese_Brazil");
+    int ValFalha[4];
+    float ValPorc[4];
+    int ValRep;
+    int ValEsc;
+    int ValQtd;
+    int ValTotal;
+    ValFalha[0] = ValFalha[1] = ValFalha[2] = ValFalha[3] = 0;
+    ValPorc[0] = ValPorc[1] = ValPorc[2] = ValPorc[3] = 0;
+    ValRep = ValEsc = ValQtd = ValTotal = 0;
+
+    printf("Sistema para Cadastrar falhas dos mouses");
+    do
+    {
+        printf("\n1-Necessita de esfera");
+        printf("\n2-Necessita de limpeza");
+        printf("\n3-Necessita de troca do cabo ou conector");
+        printf("\n4-Quebrado ou inutilizado");
+        printf("\n0-Sair do sistema");
+        printf("\nLista de falhas, Digite um dos códigos acima para cadstro: ");
+        scanf("%d", &ValEsc);
+
+        switch (ValEsc)
+        {
+        case 1:
+            printf("\nDigite a quantidade de mouses para essa falha: ");
+            scanf("%d", &ValQtd);
+            ValFalha[0] = ValFalha[0] + ValQtd;
+            break;
+        case 2:
+            printf("\nDigite a quantidade de mouses para essa falha: ");
+            scanf("%d", &ValQtd);
+            ValFalha[1] = ValFalha[1] + ValQtd;
+            break;
+        case 3:
+            printf("\nDigite a quantidade de mouses para essa falha: ");
+            scanf("%d", &ValQtd);
+            ValFalha[2] = ValFalha[2] + ValQtd;
+            break;
+        case 4:
+            printf("\nDigite a quantidade de mouses para essa falha: ");
+            scanf("%d", &ValQtd);
+            ValFalha[3] = ValFalha[3] + ValQtd;
+            break;
+        case 0:
+            ValRep = 1;
+            break;
+        default:
+            break;
+        }
+
+    } while (ValRep != 1);
+    ValTotal = ValFalha[0] + ValFalha[1] + ValFalha[2] + ValFalha[3];
+    printf("Total de Mouses defeituosos cadastrado:%d", ValTotal);
+    printf("\nSituação                                   Qtd/Porcentual");
+    printf("\n1-Necessita de esfera                       %d", ValFalha[0]);
+    printf("(%.2f %%)", (ValPorc[0] = ((ValFalha[0] * 100) / ValTotal)));
+    printf("\n2-Necessita de limpeza                      %d", ValFalha[1]);
+    printf("(%.2f %%)", (ValPorc[1] = ((ValFalha[1] * 100) / ValTotal)));
+    printf("\n3-Necessita de troca do cabo ou conector    %d", ValFalha[2]);
+    printf("(%.2f %%)", (ValPorc[2] = ((ValFalha[2] * 100) / ValTotal)));
+    printf("\n4-Quebrado ou inutilizado                   %d", ValFalha[3]);
+    printf("(%.2f %%)\n", (ValPorc[3] = ((ValFalha[3] * 100) / ValTotal)));
+}
