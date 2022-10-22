1e3f57d7f1c069598a74056628f84d89955ace6f
jeffrocha <jeff.r.silva@hotmail.com>
2022-10-22 02:01:53 -0300

    s7p07e07

---
+++ GeekUniversity/Algoritmos_e_Lógica_de_Programação_do_básico_ao_avançado/Resolução_algoritmos_Com_Linguagem_C/S7/s7p07e07.c
@@ -0,0 +1,27 @@
+/*
+Seção 07 - Exercício 07
+
+Sua organização acaba de contratar um estagiário para trabalhar no suporte de informática,
+ com a intenção de fazer um levantamento nas sucatas encontradas nesta área.
+A primeira tarefa dele é testar todos os cerca de 200 mouses que se encontram lá,
+testando e anotando o estado de cada um deles, para verificar o que se pode aproveitar deles.
+Foi requisitado que você desenvolva um programa para registrar este levantamento.
+O programa deverá receber um número indeterminado de entradas, cada uma contendo:
+um número de identificação do mouse o tipo de defeito:
+-Necessita de esfera;
+-Necessita de limpeza;
+-Necessita de troca do cabo ou conector;
+-Quebrado ou inutilizado;
+
+Uma identificação igual a zero encerra o programa. Ao final
+o programa deve emitir o seguinte relatório:
+
+Quantidade de mouses: 100
+
+Situação                                   Qtd      Porcentual
+1-Necessita de esfera                       40          40%
+2-Necessita de limpeza                      30          30%
+3-Necessita de troca do cabo ou conector    15          15%
+4-Quebrado ou inutilizado                   15          15%
+
+*/
