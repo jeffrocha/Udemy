ad0f04e238005a22a9f8f22849b62f49e5353545
jeffrocha <jeff.r.silva@hotmail.com>
2022-10-29 04:05:53 -0300

    teste

--- C/s8p01e01.c
+++
@@ -1,7 +0,0 @@
-/*
-Seção 8 - Exercício 1
-
-Faça um algoritmo que carregue um vetor de 5 elementos inteiros e em seguida armazene em um vetor apenas os números pares maiores
-que 0. No final deve mostrar os elementos do vetor na tela.
-
-*/
