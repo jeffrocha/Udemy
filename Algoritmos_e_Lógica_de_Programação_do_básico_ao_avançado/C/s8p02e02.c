1f1a589d6a83f63efaec3840d6b4cde54f40f3f8
jeffrocha <jeff.r.silva@hotmail.com>
2022-11-03 12:12:26 -0300

    s8p02e02 questão

---
+++ Algoritmos_e_Lógica_de_Programação_do_básico_ao_avançado/C/s8p02e02.c
@@ -0,0 +1,5 @@
+/*
+Seção 8 - Exercicio 2
+Eescreva um alogoritmo que leia dois vetores de 10 posições e faça a soma dos elementos de mesmo indice,
+colocando o resultado em um terceiro vetor, Mostre o vetor resultante
+*/
