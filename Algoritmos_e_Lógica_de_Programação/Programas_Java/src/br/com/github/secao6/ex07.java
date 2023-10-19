bc7f5f6559060d9af2667f4e360d976e34a6bd6b
Jefferson R Silva <49925721+jeffrocha@users.noreply.github.com>
2023-10-19 13:17:17 -0300

    Create ex07.java

---
+++ Algoritmos_e_Lógica_de_Programação/Programas_Java/src/br/com/github/secao6/ex07.java
@@ -0,0 +1,16 @@
+package br.com.github.secao6;
+
+public class ex07 {
+
+	public static void main(String[] args) {
+		/*
+		 * Desenvolva um algoritmo que:
+		 * A)Leia 4 (Quatro) números;
+		 * b)Calcule o quadrado de cada um;
+		 * C)Se o valor resultante do quadrado do terceiro for >=1000. Imprima-o e finalize
+		 * D)Caso contrario, imprima os valores lidos e seus respectivos quadrados.
+		 */
+
+	}
+
+}
