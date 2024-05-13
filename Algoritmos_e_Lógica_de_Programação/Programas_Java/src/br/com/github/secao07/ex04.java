d97223edd96cd8cffa40d14c9950cec57ccd327e
Jefferson R Silva <49925721+jeffrocha@users.noreply.github.com>
2024-05-13 18:03:54 -0300

    Create ex04.java

---
+++ Algoritmos_e_Lógica_de_Programação/Programas_Java/src/br/com/github/secao07/ex04.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+Faça um algoritmo que gere e escreva os números impares dos
+números entre 100 e 200
+*******************************************************************************/
+
+//package br.com.github.secao07;
+
+import java.util.Scanner;
+
+//public class ex01 {
+public class Main
+{
+    public static void main(String[] args) {
+    for (int i=100; i<=200 ;i++){
+        if (i %2 != 0){
+            System.out.println(i);
+        }
+    } 
+    }
+}
