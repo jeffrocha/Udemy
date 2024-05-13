3a62a6367eee192dd2897f81ca9c0cb8f552982e
Jefferson R Silva <49925721+jeffrocha@users.noreply.github.com>
2024-05-13 17:23:42 -0300

    Update ex10.java

--- Algoritmos_e_Lógica_de_Programação/Programas_Java/src/br/com/github/secao6/ex10.java
+++ Algoritmos_e_Lógica_de_Programação/Programas_Java/src/br/com/github/secao6/ex10.java
@@ -2,6 +2,24 @@ package br.com.github.secao6;
 
 import java.util.Scanner;
 
-public class ex01 {
+public class ex10 {
 
-	public static void main(String[] args) {}
+	public static void main(String[] args) {
+	int varIdade;
+	Scanner teclado=new Scanner(System.in);
+		
+	System.out.println("Digite a idade do nadador: ");
+	varIdade=teclado.nextInt();
+        
+        if(varIdade>= 5 && varIdade <= 7){
+            System.out.println("Infantil A");
+        }else if (varIdade >= 8 && varIdade <= 11){
+        	System.out.println("Infantil-B");
+        }else if (varIdade >= 12 && varIdade <= 13 ){
+        	System.out.println("Juvenil-A");
+        }else if (varIdade >= 14 && varIdade <= 17 ){
+        	System.out.println("Juvenil-B");
+        }else if (varIdade >=18 ){
+        	System.out.println("Adulto");}
+	}
+}
