6fbbcb73a0315cdbb80dc9e657cb6a5395bdad0a
Jefferson R Silva <jeff.r.silva@hotmail.com>
2025-09-08 11:58:40 -0300

    Update Main.java

--- Java/eclipse-workspace/curso_programacao/src/Main.java
+++ Java/eclipse-workspace/curso_programacao/src/Main.java
@@ -1,9 +1,17 @@
+import java.util.Locale;
 
 public class Main {
-
+	
 	public static void main(String[] args) {
-		
-		System.out.println("olá mundo");
+		Locale.setDefault(Locale.US);
+
+		double x = 10.35784;
+		System.out.println(x);
+		System.out.printf("%.2f%n", x);
+		System.out.printf("%.4f%n", x);
+		System.out.println("Resultado = "+ x + " Metros");
+		System.out.printf("Resultado = %.2f Metros%n",x);
+
 
 	}
 
