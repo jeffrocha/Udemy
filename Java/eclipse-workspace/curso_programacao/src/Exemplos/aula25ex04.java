eb699dfbfa696f1f2fd548e9aba4f0c262f8f3f9
Jefferson R Silva <jeff.r.silva@hotmail.com>
2025-09-09 15:21:15 -0300

    comandos de saída e caster

---
+++ Java/eclipse-workspace/curso_programacao/src/Exemplos/aula25ex04.java
@@ -0,0 +1,18 @@
+package Exemplos;
+
+public class aula25ex04 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b;
+		double resultado;
+		
+		a=5;
+		b=2;
+				
+		resultado=(double)a/b;
+		
+		System.out.println(resultado);		
+		
+	}
+
+}
---
+++ Java/eclipse-workspace/curso_programacao/src/Exemplos/aula25ex05.java
@@ -0,0 +1,17 @@
+package Exemplos;
+
+public class aula25ex05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a;
+		int b;
+		
+		a=5.0;
+		b=(int)a;
+		
+		System.out.println(b);
+
+	}
+
+}
