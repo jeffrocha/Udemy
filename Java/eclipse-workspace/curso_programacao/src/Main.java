4d1537e6e3128ebbd9389fc7bde9532349c285c3
Jefferson R Silva <jeff.r.silva@hotmail.com>
2025-09-09 00:53:18 -0300

    exercicio 24

--- Java/eclipse-workspace/curso_programacao/src/Main.java
+++ Java/eclipse-workspace/curso_programacao/src/Main.java
@@ -4,13 +4,16 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
-
 		double x = 10.35784;
+		String nome="Maria";
+		int idade = 31;
+		double renda= 4000.0;
 		System.out.println(x);
 		System.out.printf("%.2f%n", x);
 		System.out.printf("%.4f%n", x);
 		System.out.println("Resultado = "+ x + " Metros");
 		System.out.printf("Resultado = %.2f Metros%n",x);
+		System.out.printf("%s tem %d anos e ganha %f reais%n", nome, idade, renda);
 
 
 	}
