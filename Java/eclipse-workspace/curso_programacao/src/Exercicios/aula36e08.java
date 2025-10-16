5f62673b0f8c4a682d4de0bbcdd6f4735b3de4c3
Jefferson R Silva <49925721+jeffrocha@users.noreply.github.com>
2025-10-16 19:04:02 -0300

    Update aula36e08.java

--- Java/eclipse-workspace/curso_programacao/src/Exercicios/aula36e08.java
+++ Java/eclipse-workspace/curso_programacao/src/Exercicios/aula36e08.java
@@ -22,10 +22,37 @@
 
 package Exercicios;
 
+import java.util.Scanner;
+
 public class aula36e08 {
 
 	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		double varSalario=0;
+		double varImposto=0;
+		
+		System.out.println("Digite o salario R%:");
+		varSalario=sc.nextDouble();
+				
+		//2001-3000 = 8%
+		if (varSalario>2000 && varSalario <=3000) {
+			varImposto=varImposto+((varSalario-2000)*0.08);
+			}
 
+		//3001-4500=18%
+		if (varSalario>3000 && varSalario <=4500) {
+			varImposto=varImposto+((3000-2000)*0.08);
+			varImposto=varImposto+((varSalario-3000)*0.18);}
+		
+		//>4500=28%
+		if (varSalario>4500) {
+			varImposto=varImposto+((3000-2000)*0.08);
+			varImposto=varImposto+((4500-3000)*0.18);
+			varImposto=varImposto+((varSalario-4500)*0.28);}
+		
+		System.out.printf("Salario é de R$:%.2f%n",varSalario);
+		System.out.printf("Imposto é de R$:%.2f",varImposto);
+		sc.close();
 	}
 
 }
