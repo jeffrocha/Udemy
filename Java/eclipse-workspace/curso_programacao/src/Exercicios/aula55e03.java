78a226695cc5982f56997bda19642286c58992b2
Jefferson R Silva <49925721+jeffrocha@users.noreply.github.com>
2026-02-08 00:54:55 -0300

    Update aula55e03.java

--- Java/eclipse-workspace/curso_programacao/src/Exercicios/aula55e03.java
+++ Java/eclipse-workspace/curso_programacao/src/Exercicios/aula55e03.java
@@ -1,18 +1,22 @@
 /*
-Um posto de combustíveis deseja determinar qual de seus produtos tem a preferência de seus clientes. Escreva um algoritmo para ler o tipo de combustível abastecido, codificado da seguinte forma:
+Um posto de combustíveis deseja determinar qual de seus produtos tem a preferência de seus clientes. 
+Escreva um algoritmo para ler o tipo de combustível abastecido, codificado da seguinte forma:
 1 — Álcool
 2 — Gasolina
 3 — Diesel
 4 — Fim
-Caso o usuário informe um código inválido (fora do intervalo de 1 a 4), deve ser solicitado um novo código até que seja válido. O programa será encerrado quando o código informado for o número 4.
-Ao final, deve ser apresentada a mensagem “MUITO OBRIGADO” e a quantidade de clientes que abasteceram cada tipo de combustível.
+Caso o usuário informe um código inválido (fora do intervalo de 1 a 4), 
+deve ser solicitado um novo código até que seja válido. 
+O programa será encerrado quando o código informado for o número 4.
+Ao final, deve ser apresentada a mensagem “MUITO OBRIGADO” e a 
+quantidade de clientes que abasteceram cada tipo de combustível.
 Exemplo
 Entrada:		Saída:
 8				MUITO OBRIGADO
 1				Álcool: 1
 7				Gasolina: 2
-2				Gasolina: 2
 2				Diesel: 0
+2				
 4
 
  */
